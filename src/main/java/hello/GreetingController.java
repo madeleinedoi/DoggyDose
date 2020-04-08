537db303571517e20940b76b14c7426e82df44c0
ellabuysse <ebuysse20@holynames-sea.org>
2020-04-08 07:54:12 -0700

    worked on database

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -16,10 +16,52 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        callDatabase();
         model.addAttribute("name", name);
         return "greeting";
     }
 
+    public void callDatabase() {
+
+        // Connect to database
+        String hostName = "holynamesacademy.database.windows.net"; // update me
+        String dbName = "DoggyDose"; // update me
+        String user = "hna-admin"; // update me
+        String password = "HolyNames123"; // update me
+        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
+                + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url);
+            String schema = connection.getSchema();
+            System.out.println("Successful connection - Schema: " + schema);
+
+            System.out.println("Query data example:");
+            System.out.println("=========================================");
+
+            // Create and execute a SELECT SQL statement.
+            String selectSql = "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName "
+                    + "FROM [SalesLT].[ProductCategory] pc "
+                    + "JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid";
+
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(selectSql)) {
+
+                // Print results from select statement
+                System.out.println("Top 20 categories:");
+                while (resultSet.next())
+                {
+                    System.out.println(resultSet.getString(1) + " "
+                            + resultSet.getString(2));
+                }
+                connection.close();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
