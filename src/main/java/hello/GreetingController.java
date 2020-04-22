113462a1425f7e66a9ff9eaea8e1366f260f7b8e
maddo <maddoi@hotmail.com>
2020-04-22 07:21:33 -0700

    commentved out frontpage greeting controller

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -13,10 +13,10 @@ public class GreetingController {
     public String homepage() {
         return "HomePage";
     }
-    @GetMapping("/fronypagetemp")
-    public String homepage() {
-        return "frontpagetemp";
-    }
+//    public String () {
+//        return "frontpagetemp";
+//    }    @GetMapping("/fronypagetemp")
+//
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
